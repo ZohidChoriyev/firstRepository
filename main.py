b66fe233895d5e10a8feb7ed5b4b56ffcb061258
ChoriyevJ <97903948+ChoriyevJ@users.noreply.github.com>
2024-02-17 00:37:00 +0500

    feat: new Name class

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 
-print("Hello world")
-ism = input("Ismizni kiriting: ")
-print(f"Assalomu alaykum, {ism}!")
+class Name:
+    def __init__(self, name):
+        self.name = name
+        self.last_name = None
 
+    def get_name(self):
+        return self.name
+
+    def get_last_name(self):
+        last_name = input('Last name: ')
+        self.last_name = last_name
+
+person = Name('Jamshid')
+person.get_name()
+person.get_last_name()
